4670e17a51eaf3c4fe3f879af9774caf31258767
romshke <romshkeept+github@gmail.com>
2025-10-05 01:19:51 +0300

    Add Add meals

---
+++ Meal Planner (Java)/task/src/mealplanner/Main.java
@@ -0,0 +1,24 @@
+package mealplanner;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Which meal do you want to add (breakfast, lunch, dinner)?");
+        String category = scanner.nextLine();
+
+        System.out.println("Input the meal's name:");
+        String name = scanner.nextLine();
+
+        System.out.println("Input the ingredients:");
+        String ingredients = scanner.nextLine();
+
+        Meal meal = new Meal(category, name, ingredients);
+
+        System.out.println(meal);
+        System.out.println("The meal has been added!");
+        scanner.close();
+    }
+}
---
+++ Meal Planner (Java)/task/src/mealplanner/Meal.java
@@ -0,0 +1,52 @@
+package mealplanner;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class Meal {
+    String category;
+    String name;
+    List<String> ingredients;
+
+    public Meal(String category, String name, String ingredients) {
+        this.category = category;
+        this.name = name;
+        this.ingredients = addIngredients(ingredients);
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<String> getIngredients() {
+        return ingredients;
+    }
+
+    public List<String> addIngredients(String ingredients) {
+        return List.of(ingredients.split(","));
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+
+        result.append("Category: ").append(category).append(System.lineSeparator());
+        result.append("Name: ").append(name).append(System.lineSeparator());
+        result.append("Ingredients: ").append(System.lineSeparator());
+
+        Iterator<String> iterator = ingredients.iterator();
+
+        while (iterator.hasNext()) {
+            result.append(iterator.next());
+            if (iterator.hasNext()) {
+                result.append(System.lineSeparator());
+            }
+        }
+
+        return result.toString();
+    }
+}
